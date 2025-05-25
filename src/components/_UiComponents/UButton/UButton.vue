dae624e8b3617201c9a7f605afea58c3cb782486
PositionDreamer <nulva1@yandex.ru>
2025-05-25 14:04:53 +0500

    create universal button

---
+++ src/components/_UiComponents/UButton/UButton.vue
@@ -0,0 +1,37 @@
+<script>
+    import {computed} from "vue";
+
+    export default {
+        name: "UButton",
+        props: {
+          disabled: {
+            type: Boolean,
+            default: false
+          },
+          modifier: {
+            type: String,
+            default: ""
+          }
+        },
+        setup({modifier}) {
+
+            const classList = computed(() => {
+              const finalClassList = ["button"];
+              if(modifier === 'cancel') {
+                finalClassList.push("button--cancel");
+              }
+              return finalClassList.join(" ");
+            });
+
+            return {
+              classList
+            }
+        }
+    }
+</script>
+<template>
+    <button :class="classList" :disabled="disabled">
+        <slot></slot>
+    </button>
+</template>
+<style src="./UButton.scss" lang="scss" scoped />
