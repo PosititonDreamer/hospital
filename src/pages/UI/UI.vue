ec539ebd944354175424c5da01beb0c782371823
PositionDreamer <nulva1@yandex.ru>
2025-05-25 14:05:27 +0500

    use universal alert and button, and update styles

--- src/pages/UI/UI.vue
+++ src/pages/UI/UI.vue
@@ -1,9 +1,20 @@
 <script>
+    import Alert from "@UI/Alert/Alert.vue";
+    import {ref} from "vue";
+    import UButton from "@UI/UButton/UButton.vue";
+
     export default {
         name: "UIPage",
+        components: {UButton, Alert},
         setup() {
+            const showAlert = ref(false)
+            const showConfirm = ref(false)
+            const showWarning = ref(false)
 
             return {
+              showAlert,
+              showConfirm,
+              showWarning
             }
         }
     }
@@ -19,6 +30,12 @@
       <p class="ui__text ui__text--Bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequuntur eaque incidunt iste odit provident quis quo soluta tempora voluptatum!</p>
       <p class="ui__text ui__text--ExtraBold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequuntur eaque incidunt iste odit provident quis quo soluta tempora voluptatum!</p>
       <p class="ui__text ui__text--Black">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequuntur eaque incidunt iste odit provident quis quo soluta tempora voluptatum!</p>
+      <UButton @click="showAlert = true">Show Alert</UButton>
+      <Alert @confirm="showAlert = false" @close="showAlert = false" v-if="showAlert" message="Alert message"></Alert>
+      <UButton @click="showConfirm = true">Show Confirm</UButton>
+      <Alert type="confirm" @confirm="showConfirm = false" @close="showConfirm = false" v-if="showConfirm" message="Confirm message"></Alert>
+      <UButton @click="showWarning = true">Show Warning</UButton>
+      <Alert type="warning" @confirm="showWarning = false" @close="showWarning = false" v-if="showWarning" message="Warning message"></Alert>
     </div>
 </template>
 <style src="./UI.scss" lang="scss" scoped />
